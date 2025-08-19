23a53301fb60edf0f6f8236ea0d3d5c13f756507
Mayur K Setty <settymayurk@gmail.com>
2025-08-19 19:31:11 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './sections/Navbar';
+import Hero from './sections/Hero';
+
+function App() {
+
+  return (
+    <main className="w-max-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
