dee238a869e6b069b2850be265e72635eda1e6a1
Mayur K Setty <settymayurk@gmail.com>
2025-08-20 15:39:01 +0530

    added few more projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,48 +11,8 @@ export const navLinks = [
   },
   {
     id: 3,
-    name: 'Work',
-    href: '#work',
-  },
-  {
-    id: 4,
     name: 'Contact',
     href: '#contact',
-  },
-];
-
-export const clientReviews = [
-  {
-    id: 1,
-    name: 'Emily Johnson',
-    position: 'Marketing Director at GreenLeaf',
-    img: 'assets/review1.png',
-    review:
-      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
-  },
-  {
-    id: 2,
-    name: 'Mark Rogers',
-    position: 'Founder of TechGear Shop',
-    img: 'assets/review2.png',
-    review:
-      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
-  },
-  {
-    id: 3,
-    name: 'John Dohsas',
-    position: 'Project Manager at UrbanTech ',
-    img: 'assets/review3.png',
-    review:
-      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
-  },
-  {
-    id: 4,
-    name: 'Ether Smith',
-    position: 'CEO of BrightStar Enterprises',
-    img: 'assets/review4.png',
-    review:
-      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
   },
 ];
 
@@ -94,82 +54,82 @@ export const myProjects = [
       },
     ],
   },
-  // {
-  //   title: 'LiveDoc - Real-Time Google Docs Clone',
-  //   desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
-  //   subdesc:
-  //     'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-  //   href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
-  //   texture: '/textures/project/project2.mp4',
-  //   logo: '/assets/project-logo2.png',
-  //   logoStyle: {
-  //     backgroundColor: '#13202F',
-  //     border: '0.2px solid #17293E',
-  //     boxShadow: '0px 0px 60px 0px #2F6DB54D',
-  //   },
-  //   spotlight: '/assets/spotlight2.png',
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: 'React.js',
-  //       path: '/assets/react.svg',
-  //     },
-  //     {
-  //       id: 2,
-  //       name: 'TailwindCSS',
-  //       path: 'assets/tailwindcss.png',
-  //     },
-  //     {
-  //       id: 3,
-  //       name: 'TypeScript',
-  //       path: '/assets/typescript.png',
-  //     },
-  //     {
-  //       id: 4,
-  //       name: 'Framer Motion',
-  //       path: '/assets/framer.png',
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'CarePulse - Health Management System',
-  //   desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
-  //   subdesc:
-  //     'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
-  //   href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
-  //   texture: '/textures/project/project3.mp4',
-  //   logo: '/assets/project-logo3.png',
-  //   logoStyle: {
-  //     backgroundColor: '#60f5a1',
-  //     background:
-  //       'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
-  //     border: '0.2px solid rgba(208, 213, 221, 1)',
-  //     boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
-  //   },
-  //   spotlight: '/assets/spotlight3.png',
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: 'React.js',
-  //       path: '/assets/react.svg',
-  //     },
-  //     {
-  //       id: 2,
-  //       name: 'TailwindCSS',
-  //       path: 'assets/tailwindcss.png',
-  //     },
-  //     {
-  //       id: 3,
-  //       name: 'TypeScript',
-  //       path: '/assets/typescript.png',
-  //     },
-  //     {
-  //       id: 4,
-  //       name: 'Framer Motion',
-  //       path: '/assets/framer.png',
-  //     },
-  //   ],
-  // },
+  {
+    title: 'Byte-2-Bite AI powered generative AI Recipe App',
+    desc: 'Byte-2-Bite is an innovative recipe application that leverages the power of generative AI to create personalized recipes based on user preferences and dietary restrictions. It offers a unique blend of culinary creativity and technology.',
+    subdesc:
+      'This app is designed to revolutionize the way users discover and create recipes, making it easier to find meals that suit their tastes and nutritional needs. Built with Vite (react), TailwindCSS, and Gemini AI, Byte-2-Bite combines cutting-edge technology with a user-friendly interface. Uses RAG (Retrieval Augmented Generation) to provide users with a seamless experience in generating recipes.',
+    href: 'https://github.com/kalviumcommunity/Byte-2-Bite_MayurKSetty_S68_GenAI',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/Byte-2-Bite.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'vite',
+        path: '/assets/vite.svg',
+      },
+      {
+        id: 4,
+        name: 'Gemini AI',
+        path: '/assets/gemini.png',
+      },
+    ],
+  },
+  {
+    title: 'Attacus and Psyche - Nightwalks booking system',
+    desc: 'Attacus and Psyche is a sophisticated booking system designed for Nightwalks, a unique experience that combines nature and technology. This platform allows users to book guided night walks, enhancing their connection with the environment.',
+    subdesc:
+      'Built with Vite (react), TailwindCSS, and MongoDB, Attacus and Psyche offers a seamless booking experience. It integrates advanced features like user authentication, real-time availability checks, and secure payment processing to ensure a smooth user journey.',
+    href: 'https://github.com/mayur-driod/NightWalks',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/Logo.webp',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'vite',
+        path: '/assets/vite.svg',
+      },
+      {
+        id: 4,
+        name: 'MongoDB',
+        path: '/assets/mongoDb.png',
+      },
+    ],
+  },
   // {
   //   title: 'Horizon - Online Banking Platform',
   //   desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
